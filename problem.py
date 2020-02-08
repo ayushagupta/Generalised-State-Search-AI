b6adf996255640a08a3babd973580f6f22cb4df6
ayush1389 <mumbai.ayushgupta@gmail.com>
2020-02-09 00:00:54 +0530

    Rules, start, goal inputs done

--- problem.py
+++ problem.py
@@ -1,7 +1,8 @@
 class State:
 	def __init__(self):
-		self.dictionary = {}
+		self.d = {}
 		self.variables = []
+		self.rules = []
 
 	def input_variable_int(self):
 		lower_bound = int(input("Enter lower bound of the integer variable: "))
@@ -34,9 +35,7 @@ class State:
 		name = input("Enter name of variable: ")
 		print("1. int")
 		print("2. float")
-		print("3. char")
-		print("4. bool")
-		print("5. list")
+		print("3. list")
 		type = [int(input("Enter data type: ")), 0, 0]
 		if type[0] == 1:
 			domain = self.input_variable_int()
@@ -44,18 +43,52 @@ class State:
 		elif type[0] == 2:
 			domain = self.input_variable_float()
 
-		elif type[0] == 5:
+		else:
 			list_properties = self.input_variable_list()
 			type[1] = list_properties[0]
 			type[2] = list_properties[1]
 			domain = list_properties[2]
 
-		else: 
-			domain = 0
-
-		
-		self.dictionary[name] = [type, 0, domain, [], []]
+		self.d[name] = {'type':type, 'val':0, 'domain': domain, 'constraints': []}
 		self.variables.append(name)
 
+
+	def inputTransitionRules(self):
+		print("Rules are input in the form: a = F(x1,x2,x3,...)")
+		print("Example for using variable with name a in F: d['a']['val']")
+		num_steps = int(input("Enter number of steps in the rule: "))
+		temp_rule = []
+		for i in range (num_steps):
+			print("**Step "+str(i+1)+"**")
+			lhs = input("Enter variable a: ")
+			exp = input("Enter expression F: ")
+			temp_rule.append((lhs, exp))
+		self.rules.append(temp_rule)
 		
 
+	def inputIntegerValue(self, variable_name):
+		return int(input("Enter integer value of "+variable_name+": "))
+
+	def inputFloatValue(self, variable_name):
+		return float(input("Enter float value of "+variable_name+": "))
+
+	def inputStateValues(self):
+		for i in range (len(self.variables)):
+			if self.d[self.variables[i]]['type'][0] == 1:
+				self.d[self.variables[i]]['val'] = self.inputIntegerValue(self.variables[i])
+			elif self.d[self.variables[i]]['type'][0] == 2:
+				self.d[self.variables[i]]['val'] = self.inputFloatValue(self.variables[i])
+			else:
+				for j in range (self.d[variables[i]]['type'][2]):
+					if self.d[self.variables[i]]['type'][1] == 1: 
+						self.d[self.variables[i]]['val'][j] = self.inputIntegerValue(self.variables[i])
+					elif self.d[self.variables[i]]['type'][1] == 2:
+						self.d[self.variables[i]]['val'][j] = self.inputFloatValue(self.variables[i])
+
+	def inputStartState(self):
+		print("\nEnter initial values of the state variables:")
+		self.inputStateValues()
+
+	def inputGoalState(self):
+		print("\nEnter final values of the state variables:")
+		self.inputStateValues()
--- run.py
+++ run.py
@@ -1,13 +1,33 @@
 import problem
+from copy import deepcopy
 
 def inputVar(s):
 	num = int(input("Enter number of variables: "))
 	for i in range (num):
 		print("\n****Variable "+str(i+1)+"****")
-		s.inputVariable()
+		s.inputStateVariable()
+
+def inputRules(s):
+	num = int(input("\nEnter number of rules: "))
+	for i in range (num):
+		print("\n****Rule "+str(i+1)+"****")
+		s.inputTransitionRules()
 
 
-s = problem.State()
-inputVar(s)
-print(s.dictionary)
-print(s.variables)
+def main():
+	s = problem.State()
+	inputVar(s)
+	inputRules(s)
+	print(s.d)
+	print(s.variables)
+	print(s.rules)
+	s.inputStartState()
+	
+	g = deepcopy(s)
+	g.inputGoalState()
+
+	print(s.d)
+	print(g.d)
+
+if __name__=="__main__":
+	main()
